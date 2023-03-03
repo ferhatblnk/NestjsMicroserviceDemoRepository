8c9fdf9b177664519bc9b739e634e8fd7ef2fc7f
ferhatblnk <ferhatblnk@gmail.com>
2023-03-03 15:32:23 +0300

    Git Fixed

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -36,4 +36,4 @@ import { UsersModule } from './users/users.module';
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule {} 
